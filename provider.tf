da70d4ec5efd768c24cac84bd769eaf714b47a19
thuhoai <tr.thuhoai@gmail.com>
2022-07-14 09:52:12 +0700

    initial code

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  region  = "us-east-2"
+  version = "~> 2.63"
+   access_key = ""
+    secret_key = ""
+}
---
+++ vpc.tf
@@ -0,0 +1,17 @@
+module "vpc" {
+  source         = "terraform-aws-modules/vpc/aws"
+  version        = "2.58.0"
+  name           = "test_ecs_provisioning"
+  cidr           = "10.0.0.0/16"
+  azs            = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  tags = {
+    "env"       = "dev"
+    "createdBy" = "mkerimova"
+  }
+
+}
+
+data "aws_vpc" "main" {
+  id = module.vpc.vpc_id
+}
